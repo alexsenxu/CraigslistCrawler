1a6dd774545cea0fc9c3e1a19fcf00e78ac835ee
Sen Xu <metalmask@gmail.com>
2012-11-30 02:02:09 -0800

    udpated DataPrep.java and CraigsListCrawler.java

--- src/net/senxu/craigslistcrawler/CraigsListCrawler.java
+++ src/net/senxu/craigslistcrawler/CraigsListCrawler.java
@@ -22,7 +22,7 @@ public class CraigsListCrawler {
     public static final File urlFile=new File("./uniqueURL.txt");
     
     
-    public static void main(String[] args) {
+    public static void CraigslistCrawl(String[] args) {
         // TODO code application logic here
         
         ArrayList<String> uniqueurlList=new ArrayList<String>();
@@ -35,7 +35,23 @@ public class CraigsListCrawler {
         
         
             //step1. store search result list pages.
-                //step 1.1 check if the search result is using suggested query, if the query is without quotes, neglect this search result page
+        String pageplus="&pstart=1&b=";
+        boolean hitlistStored;
+               for (String s:queryList){
+               //read in the initial query, construct next page until their is no more pages.
+                   int pagecount=0;
+                   while (!IsHitListEmpty(s)){
+                       if (pagecount>10) s=s+pageplus+pagecount;
+                       hitlistStored=StoreHitList(s, listDir);
+                       pageplus=pageplus+10;
+                   }
+                   
+                   
+                   
+               //step 1.1 check if the search result is using suggested query, if the query is without quotes, neglect this search result page
+           
+               
+           }
         
 
             //step2. parse URL from result list pages, store URL in resultDir if it's a uniqueURL, update the unique URLfile.
@@ -51,5 +67,26 @@ public class CraigsListCrawler {
         
         
         
+    }
+    
+    public static void main(String[] args){
+        RunScheduledCrawling();
+    }
+
+    private static void RunScheduledCrawling() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    /*
+    * return true if the url s returns a webpage that doesn't have any search 
+    * result, false otherwise
+    */
+    private static boolean IsHitListEmpty(String s) {
+        return false;
+    }
+    /*
+    * return true if url s webpage was stored sucessfully, false otherwise
+    */
+    private static boolean StoreHitList(String s, File listDir) {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
--- src/net/senxu/craigslistcrawler/DataPrep.java
+++ src/net/senxu/craigslistcrawler/DataPrep.java
@@ -4,8 +4,13 @@
  */
 package net.senxu.craigslistcrawler;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,24 +18,59 @@ import java.util.ArrayList;
  */
 public class DataPrep {
     
-    public static final File phrasesFile=new File("./Dicts/nearPhrases.txt");
+    public static final String fsep=System.getProperty("file.separator");
     
-    public static ArrayList<String> LoadPOIs(){
+    public static final File POIListFile=new File("."+fsep+"Dicts"+fsep+"POIList.txt");
+    public static final File NearnessTermFile=new File("."+fsep+"Dicts"+fsep+"NearPhrases.txt");
+    
+    public static ArrayList<String> LoadFile2ArrayList(File f){
         
         ArrayList<String> ret=new ArrayList<String>();
-        
-        
-        
+        try {
+            BufferedReader br=new BufferedReader(new FileReader(f));
+            String temp;
+            while ((temp=br.readLine())!=null){
+                if (temp.startsWith("#") || temp.isEmpty()) continue;
+                else{
+                    ret.add(temp);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(DataPrep.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return ret;
     }
     
+    public static ArrayList<String> LoadPOIList(){
+        return LoadFile2ArrayList(POIListFile);
+    }
+    
+    public static ArrayList<String> LoadNearnessTerms(){
+        return LoadFile2ArrayList(NearnessTermFile);
+    }
     
     public static ArrayList<String> ConstructQuery(){
+        String prefix="http://search.yahoo.com/search?p=site%3Acraigslist.org+%22";//http://search.yahoo.com/search?p=site%3Acraigslist.org+%22near+mcdonald%27s%22
+        String suffix="%22";
         ArrayList<String> ret=new ArrayList<String>();
-        
-        
-        
+        ArrayList<String> nearnessTerms=LoadFile2ArrayList(NearnessTermFile);
+        ArrayList<String> POIList=LoadFile2ArrayList(POIListFile);
+        for (String s:POIList){
+            for (String st:nearnessTerms){
+                String temp=st+"+"+s;
+                temp=temp.replace(" ", "+");
+                temp=temp.replace("'", "%27");
+                ret.add(prefix+temp+suffix);
+            }
+        }
         return ret;
+    }
+    
+    public static void main(String[] args){
+        ArrayList<String> ret=ConstructQuery();
+        for (String s:ret){
+            System.out.println(s);
+        }
     }
     
 }
