566159e554480525ed3d36d192e9ab949b624b1f
Sen Xu <sxu@intelius.com>
2013-01-16 11:43:00 -0800

    adding addressFinder

---
+++ src/net/senxu/addressfinder/AddressFinder.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.senxu.addressfinder;
+
+/**
+ *
+ * @author sxu
+ */
+public class AddressFinder {
+    
+    AddressFinder(){};
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args){
+        
+    }
+    
+}
